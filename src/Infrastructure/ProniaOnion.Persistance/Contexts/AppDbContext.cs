86f4014c1f329e9592ad9f0910e22a1abfa11c98
MuslumKhalilov <tu6BG9HAZ@code.edu.az>
2023-12-20 06:42:24 +0400

    Contexts created. AppDbContext created

---
+++ src/Infrastructure/ProniaOnion.Persistance/Contexts/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProniaOnion.Domain.Entities;
+
+namespace ProniaOnion.Persistance.Contexts
+{
+    internal class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<ProductColor> ProductColors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().Property(p=>p.Price).HasColumnType("decimal(6,2)");
+            modelBuilder.Entity<Product>().Property(p => p.Description).IsRequired(false).HasColumnType("text");
+            modelBuilder.Entity<Product>().Property(p => p.Name).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<Product>().Property(p => p.SKU).HasMaxLength(10);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
